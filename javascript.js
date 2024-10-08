ec7cd0fc809ca7cefb614c5f1b6c6f5269c3641d
stelv29 <stelv.code@gmail.com>
2024-10-08 18:18:10 -0300

    closes #4 - implementar 4 funcoes matematicas

--- javascript.js
+++ javascript.js
@@ -1,8 +1,20 @@
-function calc(){
+function add(){
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
+    
+    var output = 0;
+    
+    switch(op){
+        case '+': output = num1 + num2;
+        break;
+        case '-': output = num1 - num2;
+        break;
+        case '*': output = num1 * num2;
+        break;
+        case '/': output = num1 / num2;
+        break;
+    }
+    
+    document.getElementById("output").value = output;
 
-    var o = num1 + num2;
-
-    document.getElementById("output").value = o;
 }
